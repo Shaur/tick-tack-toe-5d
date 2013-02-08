b7036ed4ed8a887a91a5e2136ac70d3f65456567
Kirill Efimov <kirill89@gmail.com>
2013-02-08 15:31:51 +0400

    Дописал получение игры по запросу пользователя.

--- Server/app.js
+++ Server/app.js
@@ -12,148 +12,9 @@ function collection (name) {
 	return new mongo.Collection(mongoClient, name);
 }
 
-/*function index (req, res) {
-	collection("log").insert(
-		{"dateTime": new Date ()},
-		{safe: true},
-		function (err, objects) {
-			res.send ("Logged");
-		}
-	);
-}
-
-function get (req, res) {
-	var cursor = collection("log").find();
-	cursor.sort([["dateTime", "desc"]]).limit(1);
-	cursor.nextObject(function (err, doc) {
-		if (doc != null)
-			var lastDate = new Date (doc.dateTime);
-		else
-			var lastDate = new Date ();
-		res.send (JSON.stringify ({
-			"lastDate": lastDate,
-			"lastDateSeconds": lastDate.getSeconds()
-		}));
-	});
-}
-
-function tryit (req, res) {
-	var cursor = collection("tryit").find();
-	cursor.nextObject(function (err, doc) {
-		if (doc == null) {
-			res.send (" - - - ");
-		} else {
-			res.send (" + + + ");
-		}
-	});
-}
-*/
-
-/*function findActiveGame (callback) {
-	var now = new Date (new Date () - 60000);
-	var cursor = collection("games").find({"opponent": "", "date": {$gte: now}});
-	cursor.nextObject (function (err, doc) {
-		if (doc == null) {
-			callback (null, null);
-		} else {
-			callback (doc._id, doc);
-		}
-	});
-}
-
-function createNewGame (createdBy, callback) {
-	var now = new Date ();
-	var id = new mongo.ObjectID();
-	collection("games").insert(
-		{"_id": id, "date": now, "createdBy": createdBy, "opponent": "", "field": emptyField, "move": "1"},
-		{safe: true},
-		function (err, objects) {
-			callback (id);
-		}
-	);
-}
-
-function gameStatus (gameId, userId, callback) {
-	var now = new Date (new Date () - 60000);
-	var cursor = collection("games").find({"_id": gameId});
-	cursor.nextObject(function (err, doc) {
-		if (doc == null) {
-			callback ({"status": "error"});
-		} else {
-			if (doc.opponent == "") {
-				callback ({"status": "waitOpponent"});
-			} else {
-				if (doc.date < now) {
-					callback ({"status": "timeIsOver"});
-				} else if ((doc.move == "1" && userId == doc.createdBy) || (doc.move == "2" && userId == doc.opponent)) {
-					callback ({"status": "play", "field": doc.field});
-				} else {
-					callback ({"status": "wait"});
-				}
-			}
-		}
-	});
-}
-
-function changeGameStatus (gameId, opponent, callback) {
-	var now = new Date ();
-	collection("games").update({"_id": gameId}, {$set: {"opponent": opponent, "date": now}}, {safe: true}, function () {
-		callback ();
-	});
-}
-
-function playOld (req, res) {
-	var id = req.query.id;
-	if (undefined == id) {
-		res.send ("");
-		return;
-	}
-
-	try {
-		id = new mongo.ObjectID(id);
-	} catch (err) {
-		res.send ("");
-		return;
-	}
-
-	var cursor = collection("users").find({"_id": id});
-	cursor.nextObject(function (err, doc) {
-		if (doc == null) {
-			res.send ("");
-			return;
-		}
-		//res.send (doc.device);
-		findActiveGame (function (gameId, gameInfo) {
-			if (gameId == null) {
-				createNewGame (id, function (gameId) {
-					gameStatus (gameId, id, function (result) {
-						res.send (result);
-					});
-				});
-			} else {
-				if (gameInfo.createdBy.toString () == id.toString ()) {
-					gameStatus (gameId, id, function (result) {
-						res.send (result);
-					});
-				} else {
-					changeGameStatus (gameId, id, function () {
-						gameStatus (gameId, id, function (result) {
-							res.send (result);
-						});
-					});
-				}
-			}
-		});
-		return;
-	});
-}
-*/
-
-
-
 function appInit () {
 	var app = express();
-	//work
+	
 	app.get ("/getDeviceKey", function (req, res) {
 		getDeviceKey.main (req, res, function (result) {
 			res.send (result);
@@ -165,10 +26,6 @@ function appInit () {
 		});
 	});
 
-	//test
-	//app.get ("/", index);
-	//app.get ("/get", get);
-	//app.get ("/tryit", tryit);
 	app.listen(8080);
 }
 
--- Server/play.js
+++ Server/play.js
@@ -39,14 +39,77 @@ function processQuery (userId, callback) {
 	});
 }
 
+function getUserGame (userId, callback) {
+	var minDateTime = new Date (new Date () - 30000);
+
+	var cursor = games.find ({"createdBy": userId, "date": {$gte: minDateTime}}); // Сначала ищем игру где юзер создатель игры и не прошло 30 секунд с момента последнего обновления
+	cursor.nextObject (function (err, doc) {
+		if (doc != null) {
+			callback (doc); // Вернуть игру
+			return;
+		} else {
+			var cursor = games.find ({"opponent": userId, "date": {$gte: minDateTime}}); // Затем тоже самое долько для игр где пользователь оппонент
+			cursor.nextObject (function (err, doc) {
+				if (doc != null) {
+					callback (doc); // Вернуть игру
+				} else {
+					callback (null); // Нет игры =(
+				}
+			});
+		}
+	});
+}
+
+function createNewGame (userId, callback) {
+	var now = new Date ();
+	var id = new mongo.ObjectID ();
+	games.insert (
+		{"_id": id, "date": now, "createdBy": userId, "opponent": "", "field": emptyField, "move": "1"},
+		{safe: true},
+		function (err, docs) {
+			callback (docs[0]);
+		}
+	);
+}
+
+function connectOrCreateGame (userId, callback) {
+	var minDateTime = new Date (new Date () - 30000);
+	var cursor = games.find({"opponent": "", "date": {$gte: minDateTime}});
+	cursor.nextObject (function (err, doc) {
+		if (doc == null) {
+			createNewGame (userId, callback);
+		} else {
+			var now = new Date ();
+			games.update ({"_id": doc._id}, {$set: {"opponent": userId, "date": now}}, {safe: true}, function (err, docs) {
+				getUserGame (userId, callback);
+			});
+		}
+	});
+}
+
+function getCurrentGame (userId, callback) {
+	getUserGame (userId, function (game) {
+		if (game == null) {
+			connectOrCreateGame (userId, function (game) {
+				callback (game);
+			});
+		} else {
+			callback (game);
+		}
+	});
+}
+
 function main (req, res, callback) {
 	processQuery (req.query.id, function (userId) {
 		if (userId == null) {
 			callback ("");
 			return;
 		}
-		// TODO: add game or connect game
-		callback (userId.toString ());
+
+		getCurrentGame (userId, function (game) {
+			callback (game);
+		});
+		
 		return;
 	});
 }
